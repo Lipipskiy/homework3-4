7196ff883bb009453e4beddeeb464df271dbebbc
Lipipskiy <lipskiy9696@mail.ru>
2024-07-31 21:19:51 +0300

    Add files via upload

---
+++ homework3-4.py
@@ -0,0 +1,20 @@
+
+
+def single_root_words(root_word, *other_words):
+    find_root = root_word.lower()
+    same_word = []
+    for i in other_words:
+        a = i.lower()
+        if find_root in a:
+            same_word.append(i)
+    for i in other_words:
+        a = i.lower()
+        if a in find_root:
+            same_word.append(i)
+    return same_word
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
